334ac88a2dea40643e69e99694728d5f834084f6
Dylan <60489717+DRoberts919@users.noreply.github.com>
2022-04-03 17:06:58 -0600

    append numbers

--- public/script.js
+++ public/script.js
@@ -1,12 +1,51 @@
+// variables
+const view = document.getElementById("screen");
+const equals = document.getElementById("equals");
+const keys = document.querySelector(".middle");
+
+const one = document.getElementById("one").addEventListener("click", (evt) => {
+  //   return evt.target.innerHTML
+  appendNumber(evt.target.innerHTML);
+});
+const two = document.getElementById("two").addEventListener("click", (evt) => {
+  //   return evt.target.innerHTML
+  appendNumber(evt.target.innerHTML);
+});
+
+const calculator = {
+  view: "0",
+  firstOperation: null,
+  secondOperation: null,
+  operator: null,
+};
+
 // all button operationsz
 const clear = () => {};
 
 const deleteNum = () => {};
 
-const appendNumber = (number) => {};
+const appendNumber = (number) => {
+  console.log(number);
+
+  if (calculator.operator == null) {
+    if (calculator.firstOperation == null) {
+      calculator.firstOperation = number;
+    } else if (calculator.firstOperation != null) {
+      let num = calculator.firstOperation;
+      calculator.firstOperation = `${num}${number}`;
+    }
+  } else {
+    //   set second operation
+  }
+  setDisplay();
+};
 
 const operation = (operation) => {};
 
 const calculate = () => {};
 
-const setDisplay = () => {};
+const setDisplay = () => {
+  view.innerHTML = calculator.firstOperation;
+};
+
+setDisplay();
