1cf63a58def35a61ecc023c294ec8f703ed23077
Dylan <60489717+DRoberts919@users.noreply.github.com>
2022-04-05 09:20:29 -0600

    help

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const path = require("path");
 const app = express();
 app.use(express.static(path.join(__dirname + "/public")));
 
+
+
 app.get("/", (req, res) => {
   res.sendFile("index.html");
 });
